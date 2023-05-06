836177adfa275753fb63e3f2925e02d6ef8b4af6
toxicfrank <ngugifrancis930@gmai.com>
2023-05-06 19:10:16 +0300

    ;ractice

--- 3-puts.c
+++ 3-puts.c
@@ -1,32 +1,31 @@
 #include "main.h"
-#define stdout stdout
 /**
  * _puts - function that prints a string,
  * followed by a new line, to stdout.
  * __puts - function that prints a string,
  * followed by a new line, to stdout.
- * @s: pointer to a character
- * Return: string in reverse
+ * @str: pointer to a character
+ * Return: string
  */
 
 void _puts(char *str)
 {
 	unsigned int i, count = 0, str1;
-	str1 = strlen(str);
+
+	str1 = strlen(str + 1);
 
 	for (i = 0; i < str1; i++)
 	{
 		if (*str != '\0')
 		{
-			i++;
-			count ++;
+			count++;
 		}
 	}
 	fprintf(stdout, "%s\n", str);
 }
 
-// or one can write the function below to perform function as
-//the one above
+/* or one can write the function below to perform function as */
+/* the one above */
 void __puts(char *str)
 {
 	puts(str);
--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H_
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
