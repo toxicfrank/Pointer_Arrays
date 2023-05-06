9cb0889edf6f1c2b6aadc49f5f6d85d9372066fe
toxicfrank <ngugifrancis930@gmai.com>
2023-05-06 13:24:04 +0300

    practice

---
+++ 3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse, followed by a new line.
+ * @s: pointer to a character
+ * Return: string in reverse
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
