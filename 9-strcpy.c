79257a6497b97b825e979e3c8fa79e4f81ab5af9
toxicfrank <ngugifrancis930@gmai.com>
2023-05-07 15:55:28 +0300

    practice

---
+++ 9-strcpy.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _strcpy -  function that copies
+ * the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: pointer to a char data type
+ * @src: pointer to a char data type
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+/*inster a null byte at the end of the string*/
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/*Alternaive function can also be*/
+
+/**
+ * str_cpy - function that copies
+ * the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @src: pointer to a character data type
+ * @dest: pointer to a character data type
+ * Return: pointer to dest
+ */
+char *str_cpy (char *dest, char *src)
+{
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	char *return_str = dest;
+
+	return (return_str);
+	
+}
--- main.h
+++ main.h
@@ -16,5 +16,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+char *str_cpy (char *dest, char *src)
 
 #endif
